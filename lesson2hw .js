e5f44db0e310733f877551869283a2892c11c009
Peter <pitluk22@gmail.com>
2019-12-15 15:55:46 +0300

    в файл lesson2hw добавил пару циклов

--- lesson2hw .js
+++ lesson2hw .js
@@ -27,22 +27,59 @@ var appData = {
     income: [],
     savings: false
 };
-
+/*
 for (let i = 0; i < 2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
         b = +prompt("Во сколько обойдется?", '');
     if ((typeof(a) === "string") && (typeof(a) != null) && (typeof(b) != null) 
     && (a != "") && (b != "") && a.length < 50) {
-        console.log("done");
+        console.log("Done");
     appData.expenses[a] = b;   
         } else {
-
+            console.log("Bad result");
+            i--;
     }
 };
+*/
+
+// Используем цикл WHILE
+let i = 0;
+while (i < 2){
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = +prompt("Во сколько обойдется?", '');
+    if ((typeof(a) === "string") && (typeof(a) != null) && (typeof(b) != null) 
+    && (a != "") && (b != "") && a.length < 50) {
+       console.log("Done");
+       appData.expenses[a] = b;  
+    } else {
+        console.log("Bad result");
+        i--;
+    } 
+    i++;
+};
+
+// Используем цикл DO...WHILE
+
+/*let i = 0;
+do {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = +prompt("Во сколько обойдется?", '');
+    if ((typeof(a) === "string") && (typeof(a) != null) && (typeof(b) != null) 
+    && (a != "") && (b != "") && a.length < 50) {
+       console.log("Done");
+       appData.expenses[a] = b;  
+    } else {
+        console.log("Bad result");
+        i--;
+    }
+    i++;
+}
+while (i < 2);
+*/
 
 appData.moneyPerDay = appData.budget / 30;
 
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
+alert("Ежедневный бюджет: " + appData.moneyPerDay + "Руб");
 
 if (appData.moneyPerDay < 100){
     console.log("Низкий уровень заработка");
@@ -53,7 +90,4 @@ if (appData.moneyPerDay < 100){
 } else {
     console.log("Произошла ошибка!");
 };
-
-
-
 
