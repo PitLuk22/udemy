16d036501749c4bac04282ad5ff856a4bc8f1142
Peter <pitluk22@gmail.com>
2019-12-16 15:28:35 +0300

    файл lesson2hw задание 3

--- lesson2hw .js
+++ lesson2hw .js
@@ -1,23 +1,13 @@
-/*let num = 20;
+let money, time;
 
-function showFirstMessage (text) {
-    alert (text);
-    console.log(num);
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", '');
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", '');
+    }
 }
-showFirstMessage ('Hello World!');
-console.log(num);
-*/
-
-/*function calc (a , b) {
-    return (a + b);
-}
-console.log(calc(3, 4));
-
-console.log(calc(8, 4));
-*/
-
-let money = +prompt("Ваш бюджет на месяц?", '');
-let time = prompt("Введите дату в формате YYYY-MM-DD", '');
+start();
 
 var appData = {
     budget: money,
@@ -25,24 +15,27 @@ var appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
-/*
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-        b = +prompt("Во сколько обойдется?", '');
-    if ((typeof(a) === "string") && (typeof(a) != null) && (typeof(b) != null) 
-    && (a != "") && (b != "") && a.length < 50) {
-        console.log("Done");
-    appData.expenses[a] = b;   
+
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+            b = +prompt("Во сколько обойдется?", '');
+        if ((typeof (a) === "string") && (typeof (a) != null) && (typeof (b) != null) &&
+            (a != "") && (b != "") && a.length < 50) {
+            console.log("Done");
+            appData.expenses[a] = b;
         } else {
             console.log("Bad result");
             i--;
+        }
     }
-};
-*/
+}
+chooseExpenses();
 
 // Используем цикл WHILE
+/*
 let i = 0;
 while (i < 2){
     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
@@ -57,7 +50,7 @@ while (i < 2){
     } 
     i++;
 };
-
+*/
 // Используем цикл DO...WHILE
 
 /*let i = 0;
@@ -77,17 +70,41 @@ do {
 while (i < 2);
 */
 
-appData.moneyPerDay = appData.budget / 30;
+function detectDailyBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert("Ежедневный бюджет: " + appData.moneyPerDay + " руб");
+}
+detectDailyBudget();
 
-alert("Ежедневный бюджет: " + appData.moneyPerDay + "Руб");
+function detectLevel() {
+    if (appData.moneyPerDay < 100) {
+        console.log("Низкий уровень заработка");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Средний уровень заработка");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log("Высокий уровень заработка");
+    } else {
+        console.log("Произошла ошибка!");
+    }
+}
+detectLevel();
 
-if (appData.moneyPerDay < 100){
-    console.log("Низкий уровень заработка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
-    console.log("Средний уровень заработка");
-} else if (appData.moneyPerDay > 2000){
-    console.log("Высокий уровень заработка");
-} else {
-    console.log("Произошла ошибка!");
-};
 
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?"),
+            percent = +prompt("Укажите процент по вкладу?");
+        appData.monthIncome = (save * percent / 100 / 12).toFixed();
+        alert("Доход со вклада в банке: " + appData.monthIncome + " руб");
+    }
+}
+checkSavings();
+
+function chooseOptExpenses() {
+    for(let i = 1; i <=3; i++) {
+    let questionOptExpenses = prompt ("Статья необязательных расходов?");
+    appData.optionalExpenses[i] = questionOptExpenses;
+    console.log(appData.optionalExpenses);
+    }
+}
+chooseOptExpenses();
